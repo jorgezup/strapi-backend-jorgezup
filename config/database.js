26f0119cf934406d11981e55462a3321f6cb43fd
Jorge A B Zupirolli <jorgebertolo@gmail.com>
2021-02-11 15:04:48 -0400

    database.js

--- config/database.js
+++ config/database.js
@@ -1,48 +1,16 @@
-const parse = require('pg-connection-string').parse;
-
-module.exports = ({ env }) => {
-
-  if(env('NODE_ENV') === 'production'){
-    const config = parse(process.env.DATABASE_URL);
-    return {
-      defaultConnection: 'default',
-      connections: {
-        default: {
-          connector: 'bookshelf',
-          settings: {
-            client: 'postgres',
-            // host: config.host,
-            // port: config.port,
-            // database: config.database,
-            // username: config.user,
-            // password: config.password,
-            host: process.env.DATABASE_HOST,
-            port: process.env.DATABASE_PORT,
-            database: process.env.DATABASE_NAME,
-            username: process.env.DATABASE_USER,
-            password: process.env.DATABASE_PASSWORD,
-          },
-          options: {
-            ssl: false,
-          },
-        },
+ module.exports= ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
-    }
-  }
-
-  return {
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'sqlite',
-          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-        },
-        options: {
-          useNullAsDefault: true,
-        },
+      options: {
+        useNullAsDefault: true,
       },
     },
-  }
-};
+  },
+})
+
